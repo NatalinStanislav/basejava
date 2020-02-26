4c6dff6364af5606a35831cb4f1e101efe87d117
NatalinStanislav <60177848+NatalinStanislav@users.noreply.github.com>
2020-02-27 00:39:53 +0300

    add methods realization to ArrayStorage class (HW0)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,59 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        int size = size();
+        storage[size] = r;
     }
 
     Resume get(String uuid) {
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid))
+                return storage[i];
+        }
+        System.out.println("No resume with uuid: \"" + uuid + "\"");
         return null;
     }
 
     void delete(String uuid) {
+        int num = -1;
+        int size = size();
+        for (int i = 0; i < size; i++)
+            if (storage[i].uuid.equals(uuid)) {
+                num = i;
+                break;
+            }
+        if (num == -1) {
+            System.out.println("No resume with uuid: \"" + uuid + "\"");
+            return;
+        }
+        for (int i = num; i < size; i++)
+            storage[i] = storage[i + 1];
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int size = size();
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++)
+            resumes[i] = storage[i];
+        return resumes;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage)
+            if (resume != null)
+                count++;
+        return count;
     }
 }
