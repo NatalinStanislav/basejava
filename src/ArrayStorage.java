039bec1ba84df72c576b4e79017853b096853f4d
NatalinStanislav <60177848+NatalinStanislav@users.noreply.github.com>
2020-02-27 13:03:30 +0300

    refactor methods in ArrayStorage class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,21 +3,21 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = size();
 
     void clear() {
-        int size = size();
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        int size = size();
         storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        int size = size();
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid))
                 return storage[i];
@@ -28,36 +28,39 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int num = -1;
-        int size = size();
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 num = i;
                 break;
             }
+        }
         if (num == -1) {
             System.out.println("No resume with uuid: \"" + uuid + "\"");
             return;
         }
-        for (int i = num; i < size; i++)
+        for (int i = num; i < size; i++) {
             storage[i] = storage[i + 1];
+        }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int size = size();
         Resume[] resumes = new Resume[size];
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++) {
             resumes[i] = storage[i];
+        }
         return resumes;
     }
 
     int size() {
         int count = 0;
-        for (Resume resume : storage)
+        for (Resume resume : storage) {
             if (resume != null)
                 count++;
+        }
         return count;
     }
 }
