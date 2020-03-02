61a86d2ad468d45bbe25bc09373784d377b9cf75
NatalinStanislav <60177848+NatalinStanislav@users.noreply.github.com>
2020-03-02 13:51:27 +0300

    HW2 - rename "indexInStorage" method to "getIndex"

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10_000];
     private int size = 0;
 
     public void clear() {
@@ -17,40 +17,40 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        if ((indexInStorage(resume.getUuid()) == -1) && (size < 10000)) {
+        if ((getIndex(resume.getUuid()) == -1) && (size < storage.length)) {
             storage[size] = resume;
             size++;
         } else {
-            System.out.println("Resume was not saved");
+            System.out.println("Resume with uuid: \"" + resume.getUuid() + "\"" + " was not saved");
         }
     }
 
     public void update(Resume resume) {
-        int index = indexInStorage(resume.getUuid());
+        int index = getIndex(resume.getUuid());
         if (index != -1) {
             storage[index] = resume;
         } else {
-            System.out.println("Can't update resume");
+            System.out.println("Can't update resume with uuid: \"" + resume.getUuid() + "\"");
         }
     }
 
     public Resume get(String uuid) {
-        int index = indexInStorage(uuid);
+        int index = getIndex(uuid);
         if (index == -1) {
-            System.out.println("Resume was not found");
+            System.out.println("Resume with uuid: \"" + uuid + "\"" + " was not found");
             return null;
         }
         return storage[index];
     }
 
     public void delete(String uuid) {
-        int index = indexInStorage(uuid);
+        int index = getIndex(uuid);
         if (index != -1) {
             storage[index] = storage[size - 1];
             storage[size - 1] = null;
             size--;
         } else {
-            System.out.println("Resume was not deleted");
+            System.out.println("Resume with uuid: \"" + uuid + "\"" + " was not deleted");
         }
     }
 
@@ -65,7 +65,7 @@ public class ArrayStorage {
         return size;
     }
 
-    private int indexInStorage(String uuid) {
+    private int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
