46bea83f32472fb0ee9c6d0db9f40548bc1ad2ea
NatalinStanislav <60177848+NatalinStanislav@users.noreply.github.com>
2020-03-10 14:21:20 +0300

    after Lesson4 commit

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.basejava.storage;
 
+import ru.javawebinar.basejava.exception.ExistStorageException;
+import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
@@ -8,67 +11,70 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public abstract class AbstractArrayStorage implements Storage {
-    protected static final int STORAGE_LIMIT = 10_000;
+    protected static final int STORAGE_LIMIT = 10000;
+
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
+
+    public int size() {
+        return size;
+    }
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    public void update(Resume resume) {
-        int index = getIndex(resume.getUuid());
-        if (index <= -1) {
-            System.out.println("Resume " + resume.getUuid() + " not exist");
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index < 0) {
+            throw new NotExistStorageException(r.getUuid());
         } else {
-            storage[index] = resume;
+            storage[index] = r;
         }
     }
 
-    public void save(Resume resume) {
-        int index = getIndex(resume.getUuid());
-        if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage overflow");
-        } else if (index >= 0) {
-            System.out.println("Resume " + resume.getUuid() + " already exist");
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    public void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index >= 0) {
+            throw new ExistStorageException(r.getUuid());
+        } else if (size == STORAGE_LIMIT) {
+            throw new StorageException("Storage overflow", r.getUuid());
         } else {
-            insert(resume, index);
+            insertElement(r, index);
             size++;
         }
     }
 
-    public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (index <= -1) {
-            System.out.println("Resume " + uuid + " not exist");
-            return null;
-        }
-        return storage[index];
-    }
-
     public void delete(String uuid) {
         int index = getIndex(uuid);
-        if (index <= -1) {
-            System.out.println("Resume " + uuid + " not exist");
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
         } else {
-            remove(index);
+            fillDeletedElement(index);
             storage[size - 1] = null;
             size--;
         }
     }
 
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        }
+        return storage[index];
     }
 
-    public int size() {
-        return size;
-    }
+    protected abstract void fillDeletedElement(int index);
+
+    protected abstract void insertElement(Resume r, int index);
 
     protected abstract int getIndex(String uuid);
-
-    protected abstract void insert(Resume resume, int index);
-
-    protected abstract void remove(int index);
 }
